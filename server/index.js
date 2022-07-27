12d1506af952ccd40fa470c0e41f933ef345ed95
linnamuri <lakshmi.innamuri@gmail.com>
2022-07-27 16:05:56 -0400

    added image

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-const express = require('express')
+const express = require('express');
+const { homedir } = require('os');
 const path = require('path')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname,"/../public")))
+
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.resolve( 'public/index.html'));
 });
+public/homedir.html
 
 const port = process.env.PORT || 4005
 
