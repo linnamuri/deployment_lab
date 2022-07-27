42c8ed3af2bf122cf662e735547db21a68083e05
linnamuri <lakshmi.innamuri@gmail.com>
2022-07-27 16:24:58 -0400

    changes made

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const app = express()
 app.use(express.static(path.join(__dirname,"/../public")))
 
 app.get('/',function(req,res) {
-  res.sendFile(path.resolve( __dirname,'../public/index.html'));
+  res.sendFile(path.resolve('../public/index.html'));
 });
-public/homedir.html
+
 
 const port = process.env.PORT || 4005
 
